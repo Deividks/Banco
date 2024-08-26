915fb22efde7dca2f5981bd975ffbbb08da708cf
Deivid <dkns1@aluno.ifal.edu.br>
2024-08-26 10:23:19 -0300

    Atualizações na clase Banco e adição da classe Main

--- Banco.java
+++ Banco.java
@@ -3,6 +3,15 @@ public class Banco{
     int Contas;
     
     public void criar_conta(String titular, int saldo_incial){
-        System.out.println("SEJA BEM VINDO "+titular);
+        System.out.println("SEJA BEM VINDO "+titular+", seu saldo é "+saldo_incial+"!");
+    }
+
+    public void encontrar_conta(int numero){
+        System.out.println("conta: " + numero);
+    }
+
+    public void transferir(int valor, int conta_origem, int conta_destino){
+        System.out.println("Trasferindo "+valor+" para "+conta_destino+"...");
+        System.out.println("Transferencia concluida");
     }
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args){
+        Banco meu_banco = new Banco();
+        meu_banco.Nome = "Banco Central";
+        
+
+    }
+}
