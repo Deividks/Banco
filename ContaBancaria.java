a177baf747044c8177c7770780b1a88e04f1439e
Deividks <164361430+Deividks@users.noreply.github.com>
2024-08-26 21:11:33 -0300

    alterações no main

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -8,9 +8,14 @@ public class ContaBancaria {
         System.out.println("Depositando");    
         saldo = saldo + valor;
     } 
-    public void sacar (double valor) {      
-        System.out.println("Sacar");    
-        saldo = saldo - valor;
+    public void sacar (double valor) {
+        if(saldo >= valor){
+            System.out.println("Sacando...");    
+            saldo = saldo - valor;
+            System.out.println("Saldo realizado com sucesso!");
+        }else{
+            System.out.println("Saldo insulficiente");
+        }
     } 
     public void verificar_saldo () {      
        System.out.println(saldo);
--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
 
         Banco meu_banco = new Banco();
+        ContaBancaria conta1 = new ContaBancaria();
+
         meu_banco.Nome = "Banco do Brasil";
+        System.out.print("Qual nome do titular: ");
+        String t = sc.nextLine();
+        meu_banco.criar_conta(t, 1500);
+        conta1.nome = t;
+        conta1.saldo = 1500;
+        conta1.verificar_saldo();
+
         
     }
 }
